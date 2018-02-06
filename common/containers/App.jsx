5debaa766c94f9b3dc121657cde2c55c88ad9505
Rukeith <fenglan.crazydance@gmail.com>
2018-02-06 16:07:32 +0800

    Add material ui

--- common/containers/App.jsx
+++ common/containers/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button } from 'material-ui';
 import logo from '../assets/logo.svg';
 import '../assets/App.css';
 
@@ -9,6 +10,9 @@ const App = () =>
         <img src={logo} className="App-logo" alt="logo" />
         <h1 className="App-title">Welcome to React</h1>
       </header>
+      <Button variant="raised" color="primary">
+        Primary
+      </Button>
       <p className="App-intro">
         To get started, edit <code>src/App.js</code> and save to reload.
       </p>
