f15dda8bfc1a2aa0a2a6278b458260b7ea77eacc
Rukeith <fenglan.crazydance@gmail.com>
2018-02-13 09:41:44 +0800

    Update bundle analyize config

--- webpack.common.js
+++ webpack.common.js
@@ -60,7 +60,10 @@ module.exports = {
   },
   plugins: [
     new CleanWebpackPlugin(['dist']),
-    new BundleAnalyzerPlugin({ openAnalyzer: false }),
+    new BundleAnalyzerPlugin({
+      openAnalyzer: false,
+      analyzerMode: (process.env.NODE_ENV === 'production') ? 'disabled' : 'server',
+    }),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
     }),
