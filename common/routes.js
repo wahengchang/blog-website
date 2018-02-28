9a0016cae9453c9b156637294123159c73ef8622
Rukeith <fenglan.crazydance@gmail.com>
2018-03-01 06:19:17 +0800

    Add routes

---
+++ common/routes.js
@@ -0,0 +1,57 @@
+import App from './components/App';
+import IntroPage from './containers/IntroPage';
+import ArticlePage from './containers/ArticlePage';
+import Article from './containers/Article';
+import SearchPage from './containers/SearchPage';
+import TagList from './containers/TagList';
+import TagPage from './containers/TagPage';
+import NotFoundPage from './containers/NotFoundPage';
+
+const routes = [
+  {
+    component: App,
+    routes: [
+      {
+        path: '/',
+        exact: true,
+        component: ArticlePage,
+      },
+      {
+        path: '/articles',
+        exact: true,
+        component: ArticlePage,
+        routes: [
+          {
+            path: '/articles/:articleId',
+            component: Article,
+          },
+        ],
+      },
+      {
+        path: '/intro',
+        exact: true,
+        component: IntroPage,
+      },
+      {
+        path: '/tags',
+        component: TagList,
+        routes: [
+          {
+            path: '/tags/:tagName',
+            component: TagPage,
+          },
+        ],
+      },
+      {
+        path: '/search',
+        component: SearchPage,
+      },
+      {
+        path: '*',
+        component: NotFoundPage,
+      },
+    ],
+  },
+];
+
+export default routes;
