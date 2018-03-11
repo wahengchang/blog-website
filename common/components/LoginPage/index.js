ddebe80cb05382ba6cc5e325f69973f2f2d193df
Rukeith <fenglan.crazydance@gmail.com>
2018-03-11 21:19:03 +0800

    Update login page

--- common/components/LoginPage/index.js
+++ common/components/LoginPage/index.js
@@ -1,14 +1,60 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './style.scss';
 
 const LoginPage = ({ username, password }) => (
   <main id="login-page">
     <h2>Author login</h2>
-    <p>Weclome to share knowledge</p>
-    <form>
-      <input type="text" value={username} />
-      <input type="password" value={password} />
-      <button type="submit">Login</button>
+    <p className="detail">Weclome to share knowledge</p>
+    <form
+      id="login-form"
+      acceptCharset="UTF-8"
+      encType="application/x-www-form-urlencoded"
+      method="post"
+      action={process.env.SERVER_URL || 'http://127.0.0.1:5000'}
+      name="login-form"
+      target="_top"
+    >
+      <input
+        required
+        className="login-form-item"
+        type="text"
+        placeholder="Username"
+        name="login-username"
+        form="login-form"
+        formAction={process.env.SERVER_URL || 'http://127.0.0.1:5000'}
+        formEncType="application/x-www-form-urlencoded"
+        formMethod="post"
+        formTarget="_top"
+        min="8"
+        value={username}
+      />
+      <input
+        required
+        className="login-form-item"
+        type="password"
+        name="login-password"
+        placeholder="Password"
+        form="login-form"
+        formAction={process.env.SERVER_URL || 'http://127.0.0.1:5000'}
+        formEncType="application/x-www-form-urlencoded"
+        formMethod="post"
+        formTarget="_top"
+        min="8"
+        value={password}
+      />
+      <button
+        className="login-form-item"
+        type="submit"
+        name="submit-login"
+        form="login-form"
+        formAction={process.env.SERVER_URL || 'http://127.0.0.1:5000'}
+        formEncType="application/x-www-form-urlencoded"
+        formMethod="post"
+        formTarget="_top"
+      >
+        Login
+      </button>
     </form>
   </main>
 );
